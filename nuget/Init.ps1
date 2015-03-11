5138574d2eb92158230b6b01a64479ee02aeb4f8
Sergej Popov <sergejpopov@outlook.com>
2015-03-11 17:04:53 +0000

    Added Install.ps1

---
+++ nuget/Init.ps1
@@ -0,0 +1,11 @@
+param($installPath, $toolsPath, $package, $project)
+Process 
+    {
+		$exeFileName = "IdeaSoftware.EventStore.WindowsService.exe"
+        $configPath = "$installPath\EventStore\$exeFileName.config"
+        $exePath = "$installPath\EventStore\$exeFileName"
+		$configXml = [xml] (Get-Content $configPath)
+		$node = $configXml.configuration.appSettings.add | where {$_.key -eq 'Service:EsExeLocation'}
+		$node.value = $exePath
+		$configXml.Save($configPath)
+    } 
